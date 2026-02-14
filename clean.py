bc3eb5177dc63e376babb91526cc680a44ad8f89
Aisha del Tio <adeltiodeprado@gmail.com>
2026-02-14 12:24:15 +0100

    canciones diferentes

--- clean.py
+++ clean.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import unicodedata
  
-df = pd.read_csv('spotify-2023.csv', encoding='latin-1')
-print(df.info())
+spotify = pd.read_csv('spotify-2023.csv', encoding='latin-1')
+print(spotify.info())
+
+# ¿ CUÁNTAS CANCIONES DIFERENTES HAY ?
+cantidad = spotify["track_name"].nunique()
+print(cantidad)
+
+# ¿ CUÁL ES EL ARTISTA QUE MÁS CANCIONES EN SOLITARIO TIENE ?
 
