5172759858f8b6c2af67ff254f089844c657948a
Aisha del Tio <adeltiodeprado@gmail.com>
2026-02-14 12:55:17 +0100

    artista
    artista con mas canciones en solitario

--- perfilar1.2.py
+++ perfilar1.2.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import unicodedata
  
 spotify = pd.read_csv('spotify-2023.csv', encoding='latin-1')
 print(spotify.info())
@@ -9,4 +8,9 @@ cantidad = spotify["track_name"].nunique()
 print(cantidad)
 
 # ¿ CUÁL ES EL ARTISTA QUE MÁS CANCIONES EN SOLITARIO TIENE ?
+grupos1 = spotify[spotify['artist_count'] == 1]
+resultado = grupos1.groupby('artist(s)_name').size()
+resultado = resultado.sort_values(ascending=False).head(3).reset_index()
+print(resultado)
 
+# ¿ CUAL ES EL DIA DEL MES EN EL QUE MÁS CANCIONES SE PUBLICAN ?
