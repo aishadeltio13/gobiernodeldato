15b5e2acaeda6b4a869eca87caa55c32eab2f549
Aisha del Tio <adeltiodeprado@gmail.com>
2026-02-20 11:41:28 +0100

    ejercicio1

--- perfilar1.1.py
+++ perfilar1.1.py
@@ -1,1 +1,13 @@
 # sweetviz
+
+import pandas as pd
+import sweetviz as sv
+
+# Cargar el CSV
+spotify = pd.read_csv("spotify-2023.csv")
+
+# Crear el reporte
+reporte = sv.analyze(spotify)
+
+# Generar el HTML
+reporte.show_html("reporte_spotify.html")
--- perfilar1.2.py
+++ perfilar1.2.py
@@ -26,5 +26,21 @@ mediana_total = spotify['bpm'].median()
 print(mediana_total)
 
 # ¿ CUAL ES EL VALOR MAS FRECUENTE DE danceability_% Y SU % ?
-resultado = spotify['danceability_%'].value_counts().head(1)
-print(resultado)
+valor = spotify['danceability_%'].value_counts().head(1)
+print(valor)
+porcentaje = (valor / len(spotify)) * 100
+print(porcentaje)
+
+# ¿ EN QUE POSICIÓN ESTÁ ED SHERAN EN RELACION A NÚMERO DE CANCIONES ?
+ranking = spotify['artist(s)_name'].value_counts()
+posicion = ranking.index.get_loc('Ed Sheeran') + 1
+print(posicion)
+
+# ¿ EN QUE AÑO HUBO 18 CANCIONES ?
+canciones_por_año = spotify['released_year'].value_counts()
+años_con_18 = canciones_por_año[canciones_por_año == 18]
+print(años_con_18)
+
+# ¿ CUANTAS CANCIONES HAY EN APPLE PLAYLIST ?
+total = spotify['in_apple_playlists'].sum()
+print(total)
